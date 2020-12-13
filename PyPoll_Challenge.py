42821abd42468f2a2cef1fd3c1a693c787092753
MonikaSData <mspiritova@gwmail.gwu.edu>
2020-12-13 15:53:34 -0500

    Added % formatting to output

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -102,7 +102,7 @@ with open(file_to_save, "w") as txt_file:
         c_votes_percentage = float(c_votes)/float(total_votes)*100
 
          # 6d: Print the county results to the terminal.
-        county_results = (f'{county_name}: {c_votes_percentage:.1f} ({c_votes:,})\n')
+        county_results = (f'{county_name}: {c_votes_percentage:.1f}% ({c_votes:,})\n')
         print(county_results)
          # 6e: Save the county votes to a text file.
         txt_file.write(county_results)
